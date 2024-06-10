9a3c68e7510de47d9465089cc576ed41a592e268
Boishakhi Ghosh Mukta <bgmukta11@gmail.com>
2024-06-11 00:07:06 +0200

    first commit

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,9 @@
+//var generateName = require('sillyname');
+
+//import generateName from "sillyname";
+//var sillyName = generateName();
+
+import superheroes from "superheroes";
+const name = randomsuperheroes();
+
+console.log (`I am ${name}`);
